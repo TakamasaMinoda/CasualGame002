5dbd725c4d726746357e7cc6f6fbb63ec75f842b
TakamasaMinoda <masa.0615.dks@gmail.com>
2020-10-07 19:50:31 +0900

    //移動だけつくった

---
+++ CasualGame2/Assets/StarCon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarCon : MonoBehaviour
+{
+	[SerializeField, Header("星の動く速さ")] float speed;
+
+	[SerializeField, Header("星の数")] int ChildCount = 0;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		foreach (Transform child in transform)
+		{
+			ChildCount++;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Translate(0.0f, speed * Time.deltaTime, 0.0f);
+	}
+}
